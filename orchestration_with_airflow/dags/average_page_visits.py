f538ec2c6b6172741aa59e58148359385b2be9cd
Shah Rahman <test.pinecone.user@proton.me>
2025-07-14 21:02:25 +0300

    basic DAG -step 3

--- orchestration_with_airflow/dags/average_page_visits.py
+++ orchestration_with_airflow/dags/average_page_visits.py
@@ -17,7 +17,14 @@ from airflow.operators.python import get_current_context
 def average_page_visits():
     @task
     def produce_page_visits_data():
-        pass
+        page_visits = [
+            {"id": 1, "name": "Cozy Apartment", "price": 120, "page_visits": random.randint(0, 50)},
+            {"id": 2, "name": "Luxury Condo", "price": 300, "page_visits": random.randint(0, 50)},
+            {"id": 3, "name": "Modern Studio", "price": 180, "page_visits": random.randint(0, 50)},
+            {"id": 4, "name": "Charming Loft", "price": 150, "page_visits": random.randint(0, 50)},
+            {"id": 5, "name": "Spacious Villa", "price": 400, "page_visits": random.randint(0, 50)},
+            ]
+        file_path = get_data_path()
 
     @task
     def process_page_visits_data():
