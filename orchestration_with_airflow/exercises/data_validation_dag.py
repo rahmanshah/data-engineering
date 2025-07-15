4daed4686abcbf7b4f25ca13b0a45ae58bea9443
Shah Rahman <rahman.shah@protonmail.ch>
2025-07-15 13:07:34 +0300

    validation DAG -step 1

--- orchestration_with_airflow/exercises/data_validation_dag.py
+++ orchestration_with_airflow/exercises/data_validation_dag.py
@@ -4,3 +4,11 @@ from datetime import datetime
 import os
 import json
 import random
+
+@dag(
+   "data_quality_pipeline",
+    start_date=datetime(2025, 1, 1),
+    schedule_interval='* * * * *',
+    catchup=False,
+    description="Data Quality Check DAG",
+)
