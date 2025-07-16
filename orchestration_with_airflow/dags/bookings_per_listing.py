93e2e34d676dc4363d38ea872bc9e3efa8064b4c
Shah Rahman <rahman.shah@protonmail.ch>
2025-07-16 10:55:39 +0300

    spark DAG -step 7

--- orchestration_with_airflow/dags/bookings_per_listing.py
+++ orchestration_with_airflow/dags/bookings_per_listing.py
@@ -15,41 +15,56 @@ import random
 )
 
 def bookings_spark_pipeline():
-    context = get_current_context()
-    execution_date = context["execution_date"]
 
-    file_date = execution_date.strftime("%Y-%m-%d_%H%M")
-    file_path = f"/tmp/data/bookings/{file_date}/bookings.csv"
+    @task
+    def generate_bookings():
+        context = get_current_context()
+        execution_date = context["execution_date"]
 
-    num_bookings = random.randint(30, 50)
-    bookings = []
+        file_date = execution_date.strftime("%Y-%m-%d_%H%M")
+        file_path = f"/tmp/data/bookings/{file_date}/bookings.csv"
 
-    for i in range(num_bookings):
-        booking = {
-            "booking_id": random.randint(1000, 5000),
-            "listing_id": random.choice([13913, 17402, 24328, 33332, 116268, 117203, 127652, 127860]),
-            "user_id": random.randint(1000, 5000),
-            "booking_time": execution_date.strftime("%Y-%m-%d %H:%M:%S"),
-            "status": random.choice(["confirmed", "cancelled", "pending"])
-        }
-        bookings.append(booking)
-    
-    directory = os.path.dirname(file_path)
-    if not os.path.exists(directory):
-        os.makedirs(directory)
+        num_bookings = random.randint(30, 50)
+        bookings = []
 
-    fieldnames = ["booking_id", "listing_id", "user_id", "booking_time", "status"]
+        for i in range(num_bookings):
+            booking = {
+                "booking_id": random.randint(1000, 5000),
+                "listing_id": random.choice([13913, 17402, 24328, 33332, 116268, 117203, 127652, 127860]),
+                "user_id": random.randint(1000, 5000),
+                "booking_time": execution_date.strftime("%Y-%m-%d %H:%M:%S"),
+                "status": random.choice(["confirmed", "cancelled", "pending"])
+            }
+            bookings.append(booking)
+        
+        directory = os.path.dirname(file_path)
+        if not os.path.exists(directory):
+            os.makedirs(directory)
 
-    with open(file_path, "w", newline="") as csvfile:
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writeheader()
-        for booking in bookings:
-            writer.writerow({
-                "booking_id": booking["booking_id"],
-                "listing_id": booking["listing_id"],
-                "user_id": booking["user_id"],
-                "booking_time": booking["booking_time"],
-                "status": booking["status"]
-            })
+        fieldnames = ["booking_id", "listing_id", "user_id", "booking_time", "status"]
 
-    print(f"Generated bookings data written to {file_path}")
+        with open(file_path, "w", newline="") as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+            for booking in bookings:
+                writer.writerow({
+                    "booking_id": booking["booking_id"],
+                    "listing_id": booking["listing_id"],
+                    "user_id": booking["user_id"],
+                    "booking_time": booking["booking_time"],
+                    "status": booking["status"]
+                })
+
+        print(f"Generated bookings data written to {file_path}")
+
+    spark_job = SparkSubmitOperator(
+        task_id="process_listings_and_bookings",
+        application="bookings_per_listing_spark.py",
+        name="listings_bookings_join",
+        application_args=[
+            "--listings_file", "/tmp/data/listings/{{ execution_date.strftime('%Y-%m') }}/listings.csv.gz",
+            "--bookings_file", "/tmp/data/bookings/{{ execution_date.strftime('%Y-%m-%d_%H%M') }}/bookings.csv",
+            "--output_path", "/tmp/data/bookings_per_listing/{{ execution_date.strftime('%Y-%m-%d_%H%M') }}"
+        ],
+        conn_id='spark_booking',
+    )
