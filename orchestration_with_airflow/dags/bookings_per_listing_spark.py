1f0ef351653b63b929da50637ca3e535a8881b4e
Shah Rahman <rahman.shah@protonmail.ch>
2025-07-16 11:34:44 +0300

    arguments parser initialized

--- orchestration_with_airflow/dags/bookings_per_listing_spark.py
+++ orchestration_with_airflow/dags/bookings_per_listing_spark.py
@@ -3,7 +3,11 @@ from pyspark.sql.functions import count
 import argparse
 
 def main():
-    pass 
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--listings_file", required=True, help="Path to the monthly listings file")
+    parser.add_argument("--bookings_file", required=True, help="Path to the hourly bookings file")
+    parser.add_argument("--output_path", required=True, help="Output path for the aggregated results")
+    args = parser.parse_args()
 
 
 if __name__ == '__main__':
