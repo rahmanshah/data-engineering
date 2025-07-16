135400b3166098c6c67dd95fe05cd454e59cb7a3
Shah Rahman <rahman.shah@protonmail.ch>
2025-07-16 10:23:01 +0300

    adding new dag with spark functionality

---
+++ orchestration_with_airflow/dags/bookings_per_listing.py
@@ -0,0 +1,18 @@
+from airflow.decorators import dag, task
+from airflow.operators.python import get_current_context
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
+from datetime import datetime
+import os
+import csv
+import random
+
+@dag(
+    "bookings_spark_pipeline",
+    start_date=datetime(2025, 1, 1),
+    schedule_interval="* * * * *",
+    catchup=False,
+    description="",
+)
+
+def bookings_spark_pipeline():
+    pass
