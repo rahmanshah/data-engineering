725a15f633bf5de4ebf2989fab1b75bca05da55f
Shah Rahman <rahman.shah@protonmail.ch>
2025-07-17 10:00:09 +0300

    postgres DAG -step 2

--- orchestration_with_airflow/dags/bookings_per_listing_with_postgres.py
+++ orchestration_with_airflow/dags/bookings_per_listing_with_postgres.py
@@ -16,49 +16,11 @@ from airflow.providers.postgres.hooks.postgres import PostgresHook
     description="",
 )
 
-def bookings_spark_pipeline():
+def bookings_with_postgres_spark_pipeline():
 
     @task
-    def generate_bookings():
-        context = get_current_context()
-        execution_date = context["execution_date"]
-
-        file_date = execution_date.strftime("%Y-%m-%d_%H%M")
-        file_path = f"/tmp/data/bookings/{file_date}/bookings.csv"
-
-        num_bookings = random.randint(30, 50)
-        bookings = []
-
-        for i in range(num_bookings):
-            booking = {
-                "booking_id": random.randint(1000, 5000),
-                "listing_id": random.choice([264776,264782,266037,268398,426354,427584,13913,15400 ]),
-                "user_id": random.randint(1000, 5000),
-                "booking_time": execution_date.strftime("%Y-%m-%d %H:%M:%S"),
-                "status": random.choice(["confirmed", "cancelled", "pending"])
-            }
-            bookings.append(booking)
-        
-        directory = os.path.dirname(file_path)
-        if not os.path.exists(directory):
-            os.makedirs(directory)
-
-        fieldnames = ["booking_id", "listing_id", "user_id", "booking_time", "status"]
-
-        with open(file_path, "w", newline="") as csvfile:
-            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-            writer.writeheader()
-            for booking in bookings:
-                writer.writerow({
-                    "booking_id": booking["booking_id"],
-                    "listing_id": booking["listing_id"],
-                    "user_id": booking["user_id"],
-                    "booking_time": booking["booking_time"],
-                    "status": booking["status"]
-                })
-
-        print(f"Generated bookings data written to {file_path}")
-
+    def read_bookings_from_postgres():
+        pass 
     ## creating output directory
     create_output_dir = BashOperator(
     task_id="create_output_directory",
