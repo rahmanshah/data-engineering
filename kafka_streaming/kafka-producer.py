25dd1b51ec94e5babd2895d3ad5b157a2a0ebb2e
Shah Rahman <rahman.shah@protonmail.ch>
2025-07-20 22:44:38 +0300

    kafka producer -step 1

---
+++ kafka_streaming/kafka-producer.py
@@ -0,0 +1,13 @@
+import json
+import random
+import textwrap
+import time
+from datetime import datetime
+
+from confluent_kafka import Producer
+
+def main():
+    pass 
+
+if __name__ == "__main__":
+    main()
