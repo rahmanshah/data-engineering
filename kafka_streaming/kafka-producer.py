8b6cca28bf9029ed7df617f0a35d2f6154d2c671
Shah Rahman <rahman.shah@protonmail.ch>
2025-07-20 22:47:33 +0300

    kafka producer -step 2

--- kafka_streaming/kafka-producer.py
+++ kafka_streaming/kafka-producer.py
@@ -6,6 +6,28 @@ from datetime import datetime
 
 from confluent_kafka import Producer
 
+## Function to generate random data
+def generate_order():
+    countries = [
+        "USA",
+        "Canada",
+        "UK",
+        "Germany",
+        "France",
+        "Australia",
+        "Japan",
+        "Ireland",
+    ]
+    order = {
+        "order_id": random.randint(1000, 9999),
+        "customer_id": random.randint(1, 10),
+        "total_price": round(random.uniform(20.0, 1000.0), 2),
+        "customer_country": random.choice(countries),
+        "merchant_country": random.choice(countries),
+        "order_date": datetime.now().isoformat(),
+    }
+    return order
+
 def main():
     pass 
 
