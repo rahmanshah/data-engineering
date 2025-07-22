f7d56cf404619855749ec3245fadb6e322ed2ff6
Shah Rahman <rahman.shah@protonmail.ch>
2025-07-22 10:41:18 +0300

    wiki consumer -step 3

--- kafka_streaming/wiki_stream/wiki_consumer.py
+++ kafka_streaming/wiki_stream/wiki_consumer.py
@@ -3,6 +3,13 @@ import sys
 
 from confluent_kafka import Consumer
 
+consumer_conf = {
+    "bootstrap.servers": "localhost:9092",
+    "group.id": "wiki-consumer-group",
+    "auto.offset.reset": "earliest",
+}
+kafka_topic = "wikipedia-changes"
+
 def main():
     pass 
 
