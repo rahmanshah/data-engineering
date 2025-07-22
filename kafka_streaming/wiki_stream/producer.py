131716bfffaf92925e8c0bdd0917d21c164623cd
Shah Rahman <rahman.shah@protonmail.ch>
2025-07-22 09:10:18 +0300

    wiki stream -step 3

--- kafka_streaming/wiki_stream/producer.py
+++ kafka_streaming/wiki_stream/producer.py
@@ -4,6 +4,9 @@ import textwrap
 from confluent_kafka import Producer
 from sseclient import SSEClient
 
+producer_conf = {"bootstrap.servers": "localhost:9092"}
+kafka_topic = "wikipedia-changes"
+
 def main():
     pass 
 
