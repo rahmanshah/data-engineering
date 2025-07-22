1c0c51ca5bbe4f17fb7660a07549c418e57f4014
Shah Rahman <rahman.shah@protonmail.ch>
2025-07-22 09:12:06 +0300

    wiki stream -step 4

--- kafka_streaming/wiki_stream/producer.py
+++ kafka_streaming/wiki_stream/producer.py
@@ -8,7 +8,11 @@ producer_conf = {"bootstrap.servers": "localhost:9092"}
 kafka_topic = "wikipedia-changes"
 
 def main():
-    pass 
+    url = "https://stream.wikimedia.org/v2/stream/recentchange"
+
+    print(
+        f"Starting to consume Wikipedia recent changes from {url} and produce to Kafka topic '{kafka_topic}'..."
+    )
 
 if __name__ == "__main__":
     main()
