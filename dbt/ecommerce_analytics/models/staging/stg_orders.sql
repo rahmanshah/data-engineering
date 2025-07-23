45c2f0bde0a2574987bd08d958d5ca8c4b3a898d
Shah Rahman <rahman.shah@protonmail.ch>
2025-07-23 21:36:24 +0300

    orders staging sql added

---
+++ dbt/ecommerce_analytics/models/staging/stg_orders.sql
@@ -0,0 +1,22 @@
+WITH source AS (
+    {{ source_query(source('json_files', 'orders')) }}
+),
+
+cleaned AS (
+    SELECT
+        order_id,
+        customer_id,
+        order_date,
+        status,
+        amount
+    FROM source
+    WHERE order_date IS NOT NULL
+)
+
+SELECT
+    order_id,
+    customer_id,
+    CAST(order_date AS DATE) AS order_date,
+    status,
+    CAST(amount AS DECIMAL(10,2)) AS amount
+FROM cleaned
