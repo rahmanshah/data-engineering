62cae00a02edf8aed630a1bfad789b96d7dc21cb
Shah Rahman <rahman.shah@protonmail.ch>
2025-07-23 09:37:23 +0300

    kafka connect consumer -step 2

--- kafka_streaming/kafka_connect/kafka_connect_consumer.py
+++ kafka_streaming/kafka_connect/kafka_connect_consumer.py
@@ -4,6 +4,12 @@ from decimal import Decimal
 
 from confluent_kafka import Consumer
 
+consumer_config = {
+    "bootstrap.servers": "localhost:9092",
+    "group.id": "postgres-price-consumer",
+    "auto.offset.reset": "earliest",
+}
+
 def main():
     pass 
 
