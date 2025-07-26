be15bea00a348d617036ba9c8f772f0fa15d31f2
Shah Rahman <rahman.shah@protonmail.ch>
2025-07-26 07:04:07 +0300

    PWD - Remove duplicates from a dataframe

---
+++ docker/pyspark_docker_12/dataframe_spark.py
@@ -0,0 +1,30 @@
+from pyspark.sql import SparkSession
+
+
+# Create a Spark session
+spark = SparkSession.builder \
+    .appName("DataFrameSparkApp") \
+    .getOrCreate()
+
+column_names = ["language", "framework", "users"]
+data = [
+    ("Python", "FastAPI", 20000), 
+    ("Python", "FastAPI", 9000), 
+    ("Java", "Spring", 7000), 
+    ("JavaScript", "ReactJS", 5000)
+]
+df = spark.createDataFrame(data, column_names)
+df.show()
+
+# Remove duplicates from the DataFrame
+df_cleaned = df.dropDuplicates()
+print("DataFrame after removing duplicates:")
+df_cleaned.show()
+
+# Remove duplicates based on a specific column
+df_cleaned_specific = df.dropDuplicates(["language"])
+print("DataFrame after removing duplicates based on 'language':")
+df_cleaned_specific.show()
+
+# Stop SparkSession
+spark.stop()
