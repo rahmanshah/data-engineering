d0e5863a315e7191060957c20107bedeff5a58d1
Shah Rahman <rahman.shah@protonmail.ch>
2025-07-27 10:25:29 +0300

    schema printed after added data and timestamp

--- docker/pyspark_docker_20/dataframe_spark.py
+++ docker/pyspark_docker_20/dataframe_spark.py
@@ -23,10 +23,18 @@ df_with_date = df.withColumn("current_date", current_date())
 print("DataFrame with Current Date:")
 df_with_date.show()
 
+# Show the schema of the DataFrame
+print("Schema of DataFrame:")
+df_with_date.printSchema()
+
 # Add current timestamp
 df_with_timestamp = df.withColumn("current_timestamp", current_timestamp())
 print("DataFrame with Current Timestamp:")
 df_with_timestamp.show()
+
+# Show the schema of the DataFrame
+print("Schema of DataFrame:")
+df_with_timestamp.printSchema()
 
 # Stop SparkSession
 spark.stop()
