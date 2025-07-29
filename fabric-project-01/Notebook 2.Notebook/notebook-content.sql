f0bbfa100ddb347774401502fcec8bff9e4f118f
rahmanshah <75374774+rahmanshah@users.noreply.github.com>
2025-07-29 22:36:21 +0300

    Warehouse created

---
+++ fabric-project-01/Notebook 2.Notebook/notebook-content.sql
@@ -0,0 +1,34 @@
+-- Fabric notebook source
+
+-- METADATA ********************
+
+-- META {
+-- META   "kernel_info": {
+-- META     "name": "sqldatawarehouse"
+-- META   },
+-- META   "dependencies": {
+-- META     "warehouse": {
+-- META       "default_warehouse": "e3c59275-07dc-9e1e-4074-be687fa8922e",
+-- META       "known_warehouses": [
+-- META         {
+-- META           "id": "e3c59275-07dc-9e1e-4074-be687fa8922e",
+-- META           "type": "Datawarehouse"
+-- META         }
+-- META       ]
+-- META     }
+-- META   }
+-- META }
+
+-- CELL ********************
+
+-- Welcome to your new notebook
+-- Type here in the cell editor to add code!
+SELECT COUNT(*) FROM dbo.Geography;
+
+
+-- METADATA ********************
+
+-- META {
+-- META   "language": "sql",
+-- META   "language_group": "sqldatawarehouse"
+-- META }
