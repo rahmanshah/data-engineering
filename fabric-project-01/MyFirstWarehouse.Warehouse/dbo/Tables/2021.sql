8eaf8d831580343a0f4ad9dcc7196b85c642439e
rahmanshah <75374774+rahmanshah@users.noreply.github.com>
2025-07-30 12:57:40 +0300

    Ingest data into a warehouse using data pipeline

---
+++ fabric-project-01/MyFirstWarehouse.Warehouse/dbo/Tables/2021.sql
@@ -0,0 +1,12 @@
+CREATE TABLE [dbo].[2021] (
+
+	[Column1] varchar(8000) NULL, 
+	[Column2] varchar(8000) NULL, 
+	[Column3] varchar(8000) NULL, 
+	[Column4] varchar(8000) NULL, 
+	[Column5] varchar(8000) NULL, 
+	[Column6] varchar(8000) NULL, 
+	[Column7] varchar(8000) NULL, 
+	[Column8] varchar(8000) NULL, 
+	[Column9] varchar(8000) NULL
+);
