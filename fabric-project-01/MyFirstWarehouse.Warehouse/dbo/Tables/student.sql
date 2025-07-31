5c996c120459e62f1b9a693245ed9c5ffe358f03
rahmanshah <75374774+rahmanshah@users.noreply.github.com>
2025-07-31 21:28:49 +0300

    table initialization

---
+++ fabric-project-01/MyFirstWarehouse.Warehouse/dbo/Tables/student.sql
@@ -0,0 +1,10 @@
+CREATE TABLE [dbo].[student] (
+
+	[id] varchar(10) NULL, 
+	[name] varchar(50) NULL, 
+	[birthdate] date NULL, 
+	[enrollment_year] int NULL, 
+	[major] varchar(50) NULL, 
+	[gender] varchar(10) NULL, 
+	[creditpoints] int NULL
+);
