533b9dff96c094392cf5728969f2e46f690d34e6
rahmanshah <75374774+rahmanshah@users.noreply.github.com>
2025-08-25 12:38:14 +0300

    copy activity and script activity added to add files to data warehouse and add filename to file_tracker table

---
+++ Fabric_data_migration_project/migration_dedicated.Warehouse/dbo/Tables/DimProduct.sql
@@ -0,0 +1,9 @@
+CREATE TABLE [dbo].[DimProduct] (
+
+	[ProductID] varchar(100) NULL, 
+	[ProductName] varchar(100) NULL, 
+	[Category] varchar(50) NULL, 
+	[Price] varchar(100) NULL, 
+	[FileName] varchar(100) NULL, 
+	[LoadDate] datetime2(0) NULL
+);
---
+++ Fabric_data_migration_project/migration_dedicated.Warehouse/dbo/Tables/FileTracker.sql
@@ -0,0 +1,5 @@
+CREATE TABLE [dbo].[FileTracker] (
+
+	[FileName] varchar(100) NULL, 
+	[LoadDate] datetime2(0) NULL
+);
