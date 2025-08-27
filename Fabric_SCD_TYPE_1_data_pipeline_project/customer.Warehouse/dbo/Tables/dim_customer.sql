464441563ce8f6613c5ba2138fc361a0eabd8e7f
rahmanshah <75374774+rahmanshah@users.noreply.github.com>
2025-08-27 10:04:11 +0300

    customer warehouse created along with tables to store data

---
+++ Fabric_SCD_TYPE_1_data_pipeline_project/customer.Warehouse/dbo/Tables/dim_customer.sql
@@ -0,0 +1,8 @@
+CREATE TABLE [dbo].[dim_customer] (
+
+	[customer_id] int NULL, 
+	[name] varchar(100) NULL, 
+	[email] varchar(100) NULL, 
+	[address] varchar(200) NULL, 
+	[last_updated_date] varchar(200) NULL
+);
---
+++ Fabric_SCD_TYPE_1_data_pipeline_project/customer.Warehouse/dbo/Tables/stg_customer.sql
@@ -0,0 +1,8 @@
+CREATE TABLE [dbo].[stg_customer] (
+
+	[customer_id] int NULL, 
+	[name] varchar(100) NULL, 
+	[email] varchar(100) NULL, 
+	[address] varchar(200) NULL, 
+	[file_date] varchar(200) NULL
+);
