76476e5cb3c1601c860212f302f752394733e62f
rahmanshah <75374774+rahmanshah@users.noreply.github.com>
2025-08-31 16:42:47 +0300

    setting up project lakehouse, notebook

---
+++ Fabric_sparksql_ecommerce_project/Notebook 1.Notebook/notebook-content.py
@@ -0,0 +1,34 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "25452ddb-7570-4b5d-9c05-07a17d1b8bbe",
+# META       "default_lakehouse_name": "rawDataLakehouse",
+# META       "default_lakehouse_workspace_id": "ae3e989e-5bbd-4160-9420-a1e9e48a9927",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "25452ddb-7570-4b5d-9c05-07a17d1b8bbe"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+# Welcome to your new notebook
+# Type here in the cell editor to add code!
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
