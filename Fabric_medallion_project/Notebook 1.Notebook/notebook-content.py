136ceeab8d31100d706ba846f3f78fa098168a64
rahmanshah <75374774+rahmanshah@users.noreply.github.com>
2025-09-03 13:42:19 +0300

    bronze layer dataframe created

--- Fabric_medallion_project/Notebook 1.Notebook/notebook-content.py
+++ Fabric_medallion_project/Notebook 1.Notebook/notebook-content.py
@@ -20,11 +20,54 @@
 # META   }
 # META }
 
+# MARKDOWN ********************
+
+# ### Bronze layer 
+
+# MARKDOWN ********************
+
+# #### Creating bronze layer dataframe from parquet files
+
 # CELL ********************
 
 # Welcome to your new notebook
 # Type here in the cell editor to add code!
+inventory_df = spark.read.parquet("abfss://medallion@onelake.dfs.fabric.microsoft.com/medallion.Lakehouse/Files/Bronze/inventory_data.parquet")
+orders_df = spark.read.parquet("abfss://medallion@onelake.dfs.fabric.microsoft.com/medallion.Lakehouse/Files/Bronze/orders_data.parquet")
+returns_df = spark.read.parquet("abfss://medallion@onelake.dfs.fabric.microsoft.com/medallion.Lakehouse/Files/Bronze/returns_data.xlsx.parquet")
 
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+display(inventory_df.limit(5))
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+display(orders_df.limit(5))
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+display(returns_df.limit(5))
 
 # METADATA ********************
 
