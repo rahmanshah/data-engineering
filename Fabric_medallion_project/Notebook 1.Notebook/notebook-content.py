92322e09cbc886448f017bc5c9786235bb917213
rahmanshah <75374774+rahmanshah@users.noreply.github.com>
2025-09-03 13:37:28 +0300

    added notebook

---
+++ Fabric_medallion_project/Notebook 1.Notebook/notebook-content.py
@@ -0,0 +1,34 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "1ecad65e-4e7f-41af-9e18-7e597b89575b",
+# META       "default_lakehouse_name": "medallion",
+# META       "default_lakehouse_workspace_id": "7a93d4dd-b465-4f10-845d-eb46197ecf7d",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "1ecad65e-4e7f-41af-9e18-7e597b89575b"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+# Welcome to your new notebook
+# Type here in the cell editor to add code!
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
